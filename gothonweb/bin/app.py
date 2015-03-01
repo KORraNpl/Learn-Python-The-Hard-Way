4f39d815a488a242877678c3eb74bab36016b881
KORraN <remigiusz.zetkowski@gmail.com>
2015-03-01 22:37:05 +0100

    gothonweb after exercise 51

--- gothonweb/bin/app.py
+++ gothonweb/bin/app.py
@@ -1,17 +1,21 @@
 import web
 
 urls = (
-    '/', 'Index'
+    '/hello', 'Index'
 )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
 class Index:
     def GET(self):
-        greeting = None
-        return render.foo()
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
